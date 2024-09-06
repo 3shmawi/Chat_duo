9af61a688098f6d60bf95a1487a28252c102cc3c
ashmawi <mohamedashmawy918@gmail.com>
2024-09-06 12:00:54 +0300

    initial salma commit

--- lib/app/app.dart
+++ lib/app/app.dart
@@ -6,25 +6,9 @@ class MyApp extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(
-      theme: ThemeData(
-        primaryColor: Colors.red,
-        colorScheme: const ColorScheme.light(
-          primary: Colors.red,
-        ),
-        primarySwatch: Colors.blue,
-        elevatedButtonTheme: ElevatedButtonThemeData(
-          style: ElevatedButton.styleFrom(
-            backgroundColor: Colors.red,
-            foregroundColor: Colors.white,
-            shape: RoundedRectangleBorder(
-              borderRadius: BorderRadius.circular(10),
-            ),
-            minimumSize: const Size(300, 50),
-          ),
-        ),
-      ),
-      home: const SplashScreen(),
+    return const MaterialApp(
+      debugShowCheckedModeBanner: false,
+      home: SplashScreen(),
     );
   }
 }
--- lib/screens/splash.dart
+++ lib/screens/splash.dart
@@ -1,37 +1,10 @@
 import 'package:flutter/material.dart';
 
-import 'auth/login.dart';
-
-class SplashScreen extends StatefulWidget {
+class SplashScreen extends StatelessWidget {
   const SplashScreen({super.key});
 
   @override
-  State<SplashScreen> createState() => _SplashScreenState();
-}
-
-class _SplashScreenState extends State<SplashScreen> {
-  @override
-  void initState() {
-    super.initState();
-    Future.delayed(const Duration(seconds: 2)).then(
-      (_) => Navigator.of(context).pushReplacement(
-        MaterialPageRoute(
-          builder: (context) => const LoginScreen(),
-        ),
-      ),
-    );
-  }
-
-  @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      body: Center(
-        child: Image.asset(
-          'assets/icons/logo.png',
-          height: 150,
-          width: 150,
-        ),
-      ),
-    );
+    return Scaffold();
   }
 }
