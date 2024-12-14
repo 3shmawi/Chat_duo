fe84087a94a7099f41116cd9f05cc4d7c504d51c
3shmawi <mohamedashmawy918@gmail.com>
2024-12-14 21:11:18 +0200

    start commit

--- lib/main.dart
+++ lib/main.dart
@@ -1,15 +1,13 @@
-import 'package:chat_duo/services/local_strage.dart';
 import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
 
-import 'app/app.dart';
 import 'firebase_options.dart';
+import 'my_app.dart';
 
 void main() async {
   WidgetsFlutterBinding.ensureInitialized();
   await Firebase.initializeApp(
     options: DefaultFirebaseOptions.currentPlatform,
   );
-  await CacheHelper.init();
   runApp(const MyApp());
 }
